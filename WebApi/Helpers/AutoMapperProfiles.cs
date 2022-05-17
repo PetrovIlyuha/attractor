0cc443e6d6a2db1efbfecc3c5a2fe6234571e3f1
Petrov Ilya <dnamix1@gmail.com>
2022-05-17 20:23:27 +0300

    config for automapper transformations

---
+++ WebApi/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System.Linq;
+using WebApi.DTOs;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>().ForMember(
+                dest => dest.PhotoUrl,
+                opts => opts.MapFrom(src => src.Photos.FirstOrDefault(photo => photo.IsMain).Url)
+            );
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
