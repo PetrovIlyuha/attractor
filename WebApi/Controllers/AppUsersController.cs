687c31942ec5144006eb815a531677629847ee08
Petrov Ilya <dnamix1@gmail.com>
2022-05-18 15:33:00 +0300

    started using repository pattern for users controller with automapper

--- WebApi/Controllers/AppUsersController.cs
+++ WebApi/Controllers/AppUsersController.cs
@@ -2,44 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DataAccess;
+using WebApi.DTOs;
 using WebApi.Entities;
+using WebApi.Interfaces;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     public class AppUsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
 
-        public AppUsersController(DataContext context)
+        public AppUsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
-        // GET: api/AppUsers
-        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            var users = await userRepository.GetUsersAsync();
+            var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(usersToReturn);
         }
 
-        [Authorize]
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetAppUser(int id)
+        public async Task<ActionResult<MemberDto>> GetAppUserById(int id)
         {
-            var appUser = await _context.Users.FindAsync(id);
+            var appUser = await userRepository.GetUserByIdAsync(id);
 
             if (appUser == null)
             {
                 return NotFound();
             }
 
-            return appUser;
+            return mapper.Map<MemberDto>(appUser);
+        }
+        
+        [HttpGet("username/{username}")]
+        public async Task<ActionResult<MemberDto>> GetAppUserByUsername(string username)
+        {
+            var appUser = await userRepository.GetUserByUsernameAsync(username);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return mapper.Map<MemberDto>(appUser);
         }
 
         // PUT: api/AppUsers/5
@@ -52,57 +70,57 @@ namespace WebApi.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(appUser).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AppUserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            userRepository.Update(appUser);
+            await userRepository.SaveAllAsync();
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!AppUserExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
 
             return NoContent();
         }
 
         // POST: api/AppUsers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<AppUser>> CreateUser(AppUser appUser)
-        {
-            _context.Users.Add(appUser);
-            await _context.SaveChangesAsync();
+        //[HttpPost]
+        //public async Task<ActionResult<AppUser>> CreateUser(AppUser appUser)
+        //{
+        //    _context.Users.Add(appUser);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetAppUser", new { id = appUser.Id }, appUser);
-        }
+        //    return CreatedAtAction("GetAppUser", new { id = appUser.Id }, appUser);
+        //}
 
         // DELETE: api/AppUsers/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
-        {
-            var appUser = await _context.Users.FindAsync(id);
-            if (appUser == null)
-            {
-                return NotFound();
-            }
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteUser(int id)
+        //{
+        //    var appUser = await _context.Users.FindAsync(id);
+        //    if (appUser == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Users.Remove(appUser);
-            await _context.SaveChangesAsync();
+        //    _context.Users.Remove(appUser);
+        //    await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool AppUserExists(int id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }
+        //private bool AppUserExists(int id)
+        //{
+        //    return _context.Users.Any(e => e.Id == id);
+        //}
     }
 }
