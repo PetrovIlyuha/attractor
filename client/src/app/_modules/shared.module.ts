e2bc6f0c5efcb09355a66e336d671f2aa966a805
Petrov Ilya <dnamix1@gmail.com>
2022-05-19 16:47:19 +0300

    added gallery and tabs lib & component from ngx-bootstrap

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     FontAwesomeModule,
+    NgxGalleryModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
   ],
-  exports: [BsDropdownModule, FontAwesomeModule, ToastrModule],
+  exports: [
+    BsDropdownModule,
+    TabsModule,
+    NgxGalleryModule,
+    FontAwesomeModule,
+    ToastrModule,
+  ],
 })
 export class SharedModule {}
