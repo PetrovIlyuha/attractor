3876aa4150623b0ae557659b48ac0dfcbc12fc77
Petrov Ilya <dnamix1@gmail.com>
2022-05-23 11:19:57 +0300

    scaffold for member details view with photo gallery using ngx-gallery and tabs from ngx-bootstrap

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,66 @@
+import { MembersService } from 'src/app/_services/members.service';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member.interface';
+import { ActivatedRoute } from '@angular/router';
+
+import { faMessage, faHeart } from '@fortawesome/free-solid-svg-icons';
+import {
+  NgxGalleryAnimation,
+  NgxGalleryImage,
+  NgxGalleryOptions,
+} from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
+  messageIcon = faMessage;
+  heartIcon = faHeart;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
-  constructor() { }
+  constructor(
+    private readonly membersService: MembersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadMember();
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
   }
 
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url,
+      });
+    }
+    return imageUrls;
+  }
+
+  loadMember() {
+    this.membersService
+      .getMember(this.route.snapshot.paramMap.get('username'))
+      .subscribe({
+        next: (member) => {
+          this.member = member;
+          this.galleryImages = this.getImages();
+        },
+      });
+  }
 }
