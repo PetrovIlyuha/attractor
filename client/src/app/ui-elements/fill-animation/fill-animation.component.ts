490899768ad4641d6fe069efb34686d50a6228b4
Petrov Ilya <dnamix1@gmail.com>
2022-05-23 11:12:47 +0300

    hooked up 'edit profile menu item in the user's navbar and added loading indicator integrated into ngx-spinner'

---
+++ client/src/app/ui-elements/fill-animation/fill-animation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fill-animation',
+  templateUrl: './fill-animation.component.html',
+  styleUrls: ['./fill-animation.component.css'],
+})
+export class FillAnimationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
