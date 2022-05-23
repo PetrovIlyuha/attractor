963f0cd99da2c39795f762ea31b4f2bb437b11a6
Petrov Ilya <dnamix1@gmail.com>
2022-05-23 11:17:08 +0300

    organized data storage inside the members's service without ngrx or mobx as it is likely an overkill for this particular project

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { Member } from '../_models/member.interface';
 
 @Injectable({
@@ -10,14 +10,32 @@ import { Member } from '../_models/member.interface';
 export class MembersService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
+  members: Member[] = [];
 
   getMembers() {
-    return this.http.get<Member[]>(`${this.baseUrl}/appusers`);
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(`${this.baseUrl}/appusers`).pipe(
+      map((members) => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find((m) => m.username === username);
+    if (member) return of(member);
     return this.http.get<Member>(
       `${this.baseUrl}/appusers/username/${username}`
+    );
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(`${this.baseUrl}/appusers`, member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
     );
   }
 }
