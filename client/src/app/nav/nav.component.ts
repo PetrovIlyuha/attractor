21786b8031c9bc100300d7b9ddbfdcce10ca584f
Petrov Ilya <dnamix1@gmail.com>
2022-05-25 16:06:14 +0300

    added show/hide password input use case

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,10 @@
 import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEllipsisVertical,
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user.interface';
 import { Router } from '@angular/router';
@@ -13,6 +17,10 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NavComponent implements OnInit {
   faEllipsis = faEllipsisVertical;
+  showPasswordIcon = faEye;
+  hidePasswordIcon = faEyeSlash;
+  checkPasswordIcon = this.showPasswordIcon;
+  passwordFieldType: 'password' | 'text' = 'password';
   model: any = {};
   currentUser$: Observable<User>;
   menuIconAnimating: boolean = false;
@@ -24,6 +32,15 @@ export class NavComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  togglePasswordVisibility() {
+    this.passwordFieldType =
+      this.passwordFieldType === 'password' ? 'text' : 'password';
+    this.checkPasswordIcon =
+      this.passwordFieldType === 'password'
+        ? this.showPasswordIcon
+        : this.hidePasswordIcon;
   }
 
   enableMenuIconAnimation() {
